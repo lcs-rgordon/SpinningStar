d8547fda8a4acef07f887bc5c08e5e9bd50fe6f0
Russell Gordon <github@russellgordon.ca>
2022-04-27 09:15:06 -0400

    Star now fills in when tapped.

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -11,12 +11,30 @@ struct ContentView: View {
     
     // MARK: Stored properties
     @State var emptyStarOpacity = 1.0
+    @State var filledStarOpacity = 0.0
     
     // MARK: Computed properties
     var body: some View {
         
         ZStack {
 
+            // Filled star
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.yellow)
+                .opacity(filledStarOpacity)
+                .onTapGesture {
+                    withAnimation(
+                        Animation.easeInOut(duration: 1.0)
+                    ) {
+                        // Empty star fades in
+                        emptyStarOpacity = 1.0
+                        // Filled star fades away
+                        filledStarOpacity = 0.0
+                    }
+                }
+            
             // Empty star
             Image(systemName: "star")
                 .resizable()
@@ -27,7 +45,10 @@ struct ContentView: View {
                     withAnimation(
                         Animation.easeInOut(duration: 1.0)
                     ) {
+                        // Empty star fades away
                         emptyStarOpacity = 0.0
+                        // Filled star fades in
+                        filledStarOpacity = 1.0
                     }
                 }
 
